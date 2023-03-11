9685627390565bfbd030e362259fc694f07e063b
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-03-11 14:14:50 -0700

    variables and characters for variables

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}", "~"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var combined = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +17,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var lowercaseP = confirm("Would you like lowercase characters?")
+
+if (lowercaseP == true) {
+  combined.push(...lowercase);
+}
+
+var uppercaseP = confirm("Would you like UPPERCASE characters?")
+
+if (uppercaseP == true) {
+  combined.push(...uppercase);
+}
+
+var specCharsP = confirm("Would you like special characters?")
+
+if (specCharsP == true) {
+  combined.push(...specChars);
+}
+
+var numbersP = confirm("Would you like numeric characters?")
+
+if (numbersP == true) {
+  combined.push(...numbers);
+  // console.log(combined);
+} 
