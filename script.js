73adfe7be45ac8446977cf7165d5b20f009ac373
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-03-11 14:29:53 -0700

    function generatePassword

--- script.js
+++ script.js
@@ -12,33 +12,50 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+function passwordLength(numAmount) {
+  var str = '';
+  for (var i = 0; i < numAmount; i++) {
+    str += combined[i]
+    console.log(str);
+  }
+  return str;
+}
+
+function generatePassword() {
+  var lenthP = Number(prompt("How many characters do you want in your password?  It must be between 8 and 128 characters."));
+
+  var lowercaseP = confirm("Would you like lowercase characters?")
+
+  if (lowercaseP == true) {
+    combined.push(...lowercase);
+  }
+
+  var uppercaseP = confirm("Would you like UPPERCASE characters?")
+
+  if (uppercaseP == true) {
+    combined.push(...uppercase);
+  }
+
+  var specCharsP = confirm("Would you like special characters?")
+
+  if (specCharsP == true) {
+    combined.push(...specChars);
+  }
+
+  var numbersP = confirm("Would you like numeric characters?")
+
+  if (numbersP == true) {
+    c ombined.push(...numbers);
+    // console.log(combined);
+  }
+
+  var answer = passwordLength(lenthP);
+  console.log(answer);
+
+  return answer;
+}
+
+//Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-var lowercaseP = confirm("Would you like lowercase characters?")
-
-if (lowercaseP == true) {
-  combined.push(...lowercase);
-}
-
-var uppercaseP = confirm("Would you like UPPERCASE characters?")
-
-if (uppercaseP == true) {
-  combined.push(...uppercase);
-}
-
-var specCharsP = confirm("Would you like special characters?")
-
-if (specCharsP == true) {
-  combined.push(...specChars);
-}
-
-var numbersP = confirm("Would you like numeric characters?")
-
-if (numbersP == true) {
-  combined.push(...numbers);
-  // console.log(combined);
-} 
